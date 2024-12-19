fb516973bee5a804ccf1ace93a27299197e6d7e7
Vaishak Prasad <vaishakprasad@gmail.com>
2024-12-18 19:55:04 -0500

    add versioning a la waveformtools

---
+++ .generate_version.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+
+import os
+from datetime import date
+
+source_directory = os.path.dirname(os.path.abspath(__file__))
+
+# print(source_directory)
+
+
+def write_git_version():
+    """Write the latest version number based on the last commit date
+    to the public folder and to __init__.py file"""
+
+    # print(package_directory)
+    # Open the file
+    # Get the version
+    # vers = os.popen(f'git -C {source_directory} log -1 --date=short | grep Date').read()[8:-1]
+    vers = str(date.today())
+    vers = vers.replace("-", ".")
+
+    # Write to public/version
+    with open(source_directory + "/public/version", "w") as vers_file:
+        vers_file.writelines([vers])
+
+    # write to __init__.py
+    ee = os.popen(
+        f"sed -i '/__version__/c\\__version__ = \"{vers}\"' {source_directory}/spectral/__init__.py"
+    )
+    # with open(package_directory+'/spectral/__init__.py', 'r') as init_file:
+    # with open(package_directory + "/../public/date.txt", "r") as vers_file:
+    # vers = vers_file.read()[:10]
+
+    ee = os.popen(
+        f"sed -i '/version/c\\ \tversion=\"{vers}\",' {source_directory}/setup.py"
+    )
+    print("Version", vers)
+
+    # Generate badge
+    badge = f"""<svg width="140" height="20" xmlns="http://www.w3.org/2000/svg">
+                <rect width="80" height="20" rx="0" ry="5" fill="grey" x="0" y="0" />
+                <rect width="60" height="20" fill="blue" x="80" y="0" />
+                <text x="6" y="13" fill="white" font-size="11">pypi package</text>
+                <text x="83" y="14" fill="white" font-size="11">{vers}</text>
+                </svg>
+            """
+
+    with open("docs/vers_badge.svg", "w") as svgf:
+        svgf.write(badge)
+
+    return vers
+
+
+write_git_version()
--- spectral/__init__.py
+++ spectral/__init__.py
@@ -1,1 +1,47 @@
+# Copyright (c) 2020, Vaishak Prasad
+# See [LICENSE](https://gitlab.com/vaishakp/spectral/-/blob/main/LICENSE) file for details.
+"""
+Module for the analysis and handling of numerical relativity and gravitational waveform data.
 
+Classes
+-------
+sim: Base class for NR simulations data.
+                This is needed to retrieve and handle Numerical Relativity data.
+spherical_array:    A 2D data-type.
+                    Stores and manages two-dimensional data on surfaces of spherical topology.
+modes_array: A data-type.
+             Handle and work with mode coefficients.
+
+"""
+import os
+
+package_directory = os.path.dirname(os.path.abspath(__file__))
+
+print(package_directory)
+
+
+def read_git_version():
+    """Get the latest version number based on the last commit date,
+    iff this is a git repo."""
+
+    # print(package_directory)
+    # Open the file
+    vers = "-1"
+    try:
+        with open(
+            package_directory + "/../public/version", "r", encoding="utf-8"
+        ) as vers_file:
+            vers = vers_file.readlines()[0]
+    except Exception as excep:
+        print(
+            excep,
+            "\n",
+            "This is not a git repo! please use the version attribute instead!",
+        )
+    # with open(package_directory + "/../public/date.txt", "r") as vers_file:
+    # vers = vers_file.read()[:10]
+
+    return vers
+
+
+__version__ = "2024.12.18"
